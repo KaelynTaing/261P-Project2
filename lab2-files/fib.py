c63f47c6b5965144375e8987c8fbe565364961ef
give-kk-five <110738266+KaelynTaing@users.noreply.github.com>
2026-02-16 16:17:06 -0800

    delete_min needs fixing

--- lab2-files/fib.py
+++ lab2-files/fib.py
@@ -1,5 +1,6 @@
 # explanations for member functions are provided in requirements.py
 from __future__ import annotations
+import math
 
 
 class FibNode:
@@ -25,7 +26,10 @@ class FibNode:
 class FibHeap:
     def __init__(self):
         # you may define any additional member variables you need
+        # added for log calculation
         self.totalNodes = 0
+        # added as pointer to keep track of min node
+        self.min = None
         self.roots = []
         pass
 
@@ -36,30 +40,102 @@ class FibHeap:
         self.totalNodes += 1
         newnode = FibNode(val)
         self.roots.append(FibNode(val))
+        if len(self.roots) == 1:
+            self.min = newnode
+        # update min
+        if newnode.get_value_in_node() < self.min.get_value_in_node():
+            self.min = newnode
         return newnode
 
     def delete_min(self) -> None:
+        # be able to reference minnode
         minnode = self.find_min()
-        for child in minnode.children:
+        # print(minnode.val)
+        # print(minnode.get_children())
+
+        # if it has no children, nothing to merge up
+        # merge children to root list
+        for child in minnode.get_children():
+            print(minnode.get_children())
+            # set flags to False
+            child.get_flag = False
             self.roots.append(child)
-        
-        arr = [None] * math.log(self.totalNodes) + 1
-        for root in self.roots:
-            idx = len(root.children)
+
+        # remove minnode from roots
+        self.roots.remove(minnode)
+
+        # array to hold nodes' number of children
+        arr = [None] * (math.ceil(math.log(self.totalNodes)) + 1)
+
+        # merge step between trees
+        for root in self.get_roots():
+            print(root.get_value_in_node())
+            # make index number of children
+            idx = len(root.get_children())
+
+            # appendedNode default as root
+            appendedNode = root
             while arr[idx] is not None:
                 # combine root at that arr[idx] and this root and check again
-                if root.val < arr[idx].val:
+                if appendedNode.get_value_in_node() < arr[idx].get_value_in_node():
+                    appendedNode = root
                     # make root child of arr[idx]
+                    arr[idx].get_children().append(root)
                 else:
-                    
-            arr[idx] = root
+                    appendedNode = arr[idx]
+                    # make arr[idx] child of root
+                    root.get_children().append(arr[idx])
 
+                # set combined to correct idx based off root
+                # remove node at this current index
+                arr[idx] = None
+                # update idx
+                idx = len(appendedNode.get_children())
+                # set new tree to that point in the array, if its None, if not go back in this while loop
+                arr[idx] = appendedNode
+
+            # arr[idx] = appendedNode
+
+        for a in arr:
+            if a is not None:
+                print("check")
+                print(a.get_value_in_node())
+
+        # set min
+        minroot = self.get_roots()[0]
+        for root in self.get_roots():
+            if root.get_value_in_node() < minroot.get_value_in_node():
+                minroot = root
+        self.min = minroot
+        # set new pointer for min to roots?
 
     def find_min(self) -> FibNode:
-        return min(self.roots)
+        # minroot = self.roots[0]
+        # for root in self.roots:
+        #     if minroot.get_value_in_node() >= root.get_value_in_node():
+        #         minroot = root
+
+        # return minroot
+        return self.min
 
     def decrease_priority(self, node: FibNode, new_val: int) -> None:
-        pass
+        # make sure node is a root node
+        if node.get_children == []:
+            node.get_value_in_node = new_val
+        else:
+            parentnode = node.parent
+            while parentnode not in self.get_roots:
+                if parentnode.get_flag is False:
+                    parentnode.get_flag = True
+                else:
+                    grandparentnode = parentnode.parent
+                    parentnode.insert(parentnode.get_value_in_node)
+                    parentnode = grandparentnode
+
+            # move node up
+            node.insert(new_val)
+
+        # also have to update min here
 
     # feel free to define new methods in addition to the above
     # fill in the definitions of each required member function (above),
